7ab4f619ba8e4fdca2c6adbbcfbde5498a95d353
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2013-12-21 17:48:59 +0200

    Add gate action stubs.

--- lib/TransactPRO/Gate/Gate.php
+++ lib/TransactPRO/Gate/Gate.php
@@ -2,7 +2,98 @@
 
 namespace TransactPRO\Gate;
 
+use TransactPRO\Gate\Exceptions\NotImplementedAction;
+
+/**
+ * @package TransactPRO\Gate
+ */
 class Gate
 {
+    /**
+     * @var array
+     */
+    private $merchantData;
 
+    /**
+     * @param array $merchantData <p>
+     * Merchant access data
+     * <code>array('guid' => '', 'pwd' => '');</code>
+     * </p>
+     */
+    function __construct(array $merchantData)
+    {
+        $this->merchantData = $merchantData;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMerchantData()
+    {
+        return $this->merchantData;
+    }
+
+    /**
+     * @throws Exceptions\NotImplementedAction
+     */
+    public function init()
+    {
+        throw new NotImplementedAction();
+    }
+
+    /**
+     * @throws Exceptions\NotImplementedAction
+     */
+    public function charge()
+    {
+        throw new NotImplementedAction();
+    }
+
+    /**
+     * @throws Exceptions\NotImplementedAction
+     */
+    public function initDms()
+    {
+        throw new NotImplementedAction();
+    }
+
+    /**
+     * @throws Exceptions\NotImplementedAction
+     */
+    public function makeHold()
+    {
+        throw new NotImplementedAction();
+    }
+
+    /**
+     * @throws Exceptions\NotImplementedAction
+     */
+    public function chargeHold()
+    {
+        throw new NotImplementedAction();
+    }
+
+    /**
+     * @throws Exceptions\NotImplementedAction
+     */
+    public function cancelDms()
+    {
+        throw new NotImplementedAction();
+    }
+
+    /**
+     * @throws Exceptions\NotImplementedAction
+     */
+    public function refund()
+    {
+        throw new NotImplementedAction();
+    }
+
+    /**
+     * @throws Exceptions\NotImplementedAction
+     */
+    public function statusRequest()
+    {
+        throw new NotImplementedAction();
+    }
 } 
--- tests/GateTest.php
+++ tests/GateTest.php
@@ -2,11 +2,89 @@
 
 namespace TransactPRO\Gate\tests;
 
+use TransactPRO\Gate\Gate;
+
 class GateTest extends \PHPUnit_Framework_TestCase
 {
-    public function testTrue()
+    /** @var Gate */
+    protected $gate;
+    /** @var array */
+    protected $merchantData;
+
+    protected function setUp()
     {
-        $this->assertTrue(true);
+        $this->merchantData = array('guid' => 'AAAA-AAAA-AAAA-AAAA', 'pwd' => '111');
+        $this->gate         = new Gate($this->merchantData);
+        parent::setUp();
+    }
+
+    public function testItCanBeInitializedWithValidMerchantDataWithoutError()
+    {
+        $this->assertEquals($this->merchantData, $this->gate->getMerchantData());
+    }
+
+    /**
+     * @expectedException \TransactPRO\Gate\Exceptions\NotImplementedAction
+     */
+    public function testInitAreNotImplemented()
+    {
+        $this->gate->init();
+    }
+
+    /**
+     * @expectedException \TransactPRO\Gate\Exceptions\NotImplementedAction
+     */
+    public function testChargeAreNotImplemented()
+    {
+        $this->gate->charge();
+    }
+
+    /**
+     * @expectedException \TransactPRO\Gate\Exceptions\NotImplementedAction
+     */
+    public function testInitDmsAreNotImplemented()
+    {
+        $this->gate->initDms();
+    }
+
+    /**
+     * @expectedException \TransactPRO\Gate\Exceptions\NotImplementedAction
+     */
+    public function testMakeHoldAreNotImplemented()
+    {
+        $this->gate->makeHold();
+    }
+
+    /**
+     * @expectedException \TransactPRO\Gate\Exceptions\NotImplementedAction
+     */
+    public function testChargeHoldAreNotImplemented()
+    {
+        $this->gate->chargeHold();
+    }
+
+    /**
+     * @expectedException \TransactPRO\Gate\Exceptions\NotImplementedAction
+     */
+    public function testCancelDmsAreNotImplemented()
+    {
+        $this->gate->cancelDms();
+    }
+
+    /**
+     * @expectedException \TransactPRO\Gate\Exceptions\NotImplementedAction
+     */
+    public function testRefundAreNotImplemented()
+    {
+        $this->gate->refund();
+    }
+
+    /**
+     * @expectedException \TransactPRO\Gate\Exceptions\NotImplementedAction
+     */
+    public function testStatusRequestAreNotImplemented()
+    {
+        $this->gate->statusRequest();
     }
 }
  
