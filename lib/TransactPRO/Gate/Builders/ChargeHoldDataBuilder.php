024b967243654c1e907fefb58a58bad512ac09e0
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2013-12-22 17:05:06 +0200

    Add ChargeHold DataBuilder

---
+++ lib/TransactPRO/Gate/Builders/ChargeHoldDataBuilder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TransactPRO\Gate\Builders;
+
+class ChargeHoldDataBuilder extends Builder
+{
+    public function build()
+    {
+        return array(
+            'init_transaction_id' => $this->getField('init_transaction_id')
+        );
+    }
+
+    protected function checkData()
+    {
+        $this->checkMandatoryField('init_transaction_id');
+    }
+}
---
+++ tests/Builders/ChargeHoldDataBuilderTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace TransactPRO\Gate\tests\Builders;
+
+class ChargeHoldDataBuilderTest extends BuilderTestCase
+{
+    protected function setUp()
+    {
+        $this->builderClass = 'TransactPRO\Gate\Builders\ChargeHoldDataBuilder';
+        $this->data = array(
+            'init_transaction_id' => '13hpf5rp1e0ss72dypjnhalzn1wmrkfmsjtwzocg'
+        );
+        $this->buildData = $this->data;
+    }
+
+    public function getMandatoryFields()
+    {
+        return array(
+            array('init_transaction_id')
+        );
+    }
+
+    public function testNonMandatoryFields()
+    {
+    }
+
+    public function getNonMandatoryFields()
+    {
+    }
+}
+ 
