bc06b13999bbdbc80ba62edf264897016c614550
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2014-01-21 16:55:35 +0200

    Fix Deprecetion bug with CURLOPT_SSL_VERIFYHOST

--- lib/TransactPRO/Gate/Request/RequestExecutor.php
+++ lib/TransactPRO/Gate/Request/RequestExecutor.php
@@ -38,7 +38,7 @@ class RequestExecutor implements RequestExecutorInterface
         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
         curl_setopt($ch, CURLOPT_POST, 1);
         if ($this->verifySSL) {
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
         }
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
