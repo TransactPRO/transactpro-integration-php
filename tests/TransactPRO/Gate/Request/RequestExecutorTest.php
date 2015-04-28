4249d3945bdfdad3c2aaf0b9d5bf2a4d58d14da2
Vitalijs Litvinovs <ulow@inbox.lv>
2015-04-28 11:28:30 +0300

    Update RequestExecutorTest.php

--- tests/TransactPRO/Gate/Request/RequestExecutorTest.php
+++ tests/TransactPRO/Gate/Request/RequestExecutorTest.php
@@ -6,16 +6,20 @@ use TransactPRO\Gate\Request\RequestExecutor;
 
 class RequestExecutorTest extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        $this->requestExecutor = new RequestExecutor('https://www.payment-api.lv', false);
+    }
+    
     public function testExecuteReturnResponse()
     {
-        $requestExecutor = new RequestExecutor('https://www.payment-api.lv', false);
-        $response = $requestExecutor->executeRequest('action', array());
+        $response = $this->requestExecutor->executeRequest('action', array());
         $this->assertInstanceOf('TransactPRO\Gate\Response\Response', $response);
     }
     
     public function testConstructApiUrl()
     {
-        $this->assertEquals('https://www.payment-api.lv/gwprocessor2.php?a=action', $this->constructApiUrl('action'));
+        $this->assertEquals('https://www.payment-api.lv/gwprocessor2.php?a=action', $this->requestExecutor->constructApiUrl('action'));
     }
 }
  
