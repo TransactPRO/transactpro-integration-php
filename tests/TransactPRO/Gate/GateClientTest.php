0d5ecf114bd8d550bfcd65f9053aeedba7456bfe
Vitalijs Litvinovs <dev@codin.pro>
2015-08-04 14:37:22 +0300

    Update GateClientTest.php

--- tests/TransactPRO/Gate/GateClientTest.php
+++ tests/TransactPRO/Gate/GateClientTest.php
@@ -172,6 +172,7 @@ class GateClientTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('TransactPRO\Gate\Response\Response', $response, 'Result must be instance of TransactPRO\Gate\Response\Response class.');
         $this->assertFalse($response->isSuccessful(), 'Response must be unsuccessful');
         $this->assertContains("resolve host", $response->getResponseContent());
+        $this->assertFalse(true, true);
     }
 }
  
