da43dbfa5f3cc57788ee46d574f8bf0c2028d395
Olga Zdančuka <oz@tpro.lv>
2019-08-05 12:29:38 +0300

    Deny to send amount_to_refund for Cancel DMS transaction

--- lib/TransactPRO/Gate/Builders/CancelDmsDataBuilder.php
+++ lib/TransactPRO/Gate/Builders/CancelDmsDataBuilder.php
@@ -2,6 +2,22 @@
 
 namespace TransactPRO\Gate\Builders;
 
-class CancelDmsDataBuilder extends RefundDataBuilder
+class CancelDmsDataBuilderr extends Builder
 {
-} 
+    public function build()
+    {
+        $result = array(
+            'init_transaction_id' => $this->getField('init_transaction_id'),
+        );
+
+        if (isset($this->data['merchant_transaction_id'])) $result['merchant_transaction_id'] = $this->getField('merchant_transaction_id');
+        if (isset($this->data['details'])) $result['details'] = $this->getField('details');
+
+        return $result;
+    }
+
+    protected function checkData()
+    {
+        $this->checkMandatoryField('init_transaction_id');
+    }
+}
