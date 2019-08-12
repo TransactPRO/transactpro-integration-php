450037daa5e2a18df6fd3aebbc85ad6d72d3e537
Olga Zdančuka <oz@tpro.lv>
2019-08-12 15:31:58 +0300

    CancelDmsDataBuilder extends RefundDataBuilder

--- lib/TransactPRO/Gate/Builders/CancelDmsDataBuilder.php
+++ lib/TransactPRO/Gate/Builders/CancelDmsDataBuilder.php
@@ -2,22 +2,6 @@
 
 namespace TransactPRO\Gate\Builders;
 
-class CancelDmsDataBuilderr extends Builder
+class CancelDmsDataBuilder extends RefundDataBuilder
 {
-    public function build()
-    {
-        $result = array(
-            'init_transaction_id' => $this->getField('init_transaction_id'),
-        );
-
-        if (isset($this->data['merchant_transaction_id'])) $result['merchant_transaction_id'] = $this->getField('merchant_transaction_id');
-        if (isset($this->data['details'])) $result['details'] = $this->getField('details');
-
-        return $result;
-    }
-
-    protected function checkData()
-    {
-        $this->checkMandatoryField('init_transaction_id');
-    }
 }
